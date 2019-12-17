ea6b7638524ccf3bd4029f554c9a1249ea005fb3
Shohan <shohan@Shohans-iMac-Pro.local>
2019-12-17 04:10:59 -0500

    Updates

---
+++ precache-manifest.b74e0c3200ccba489268d8d0a2062a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63c5fa353ff68068b5fd29e9aa08501",
+    "url": "/rolodex_react/index.html"
+  },
+  {
+    "revision": "eff45bf1a78254d70809",
+    "url": "/rolodex_react/static/css/main.ebd1d073.chunk.css"
+  },
+  {
+    "revision": "cab8dcbe7a915a79a96a",
+    "url": "/rolodex_react/static/js/2.626dfc30.chunk.js"
+  },
+  {
+    "revision": "eff45bf1a78254d70809",
+    "url": "/rolodex_react/static/js/main.4ea2c43d.chunk.js"
+  },
+  {
+    "revision": "8675b81406e9bc5cdaa9",
+    "url": "/rolodex_react/static/js/runtime-main.783e90a9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/rolodex_react/static/media/logo.25bf045c.svg"
+  }
+]);
